743ea53bbf80f61f48f621f9d240509863ad5153
GurshawnL <58784041+GurshawnL@users.noreply.github.com>
2022-08-14 13:26:16 -0400

    swapped to redis + started prisma

--- graphql/utils/pubsub.js
+++ graphql/utils/pubsub.js
@@ -1,6 +1,12 @@
-// swap for redis pubsub
-import { PubSub } from "graphql-subscriptions";
+// create redis apollo pubsub with host "127.0.0.1" and port 6379
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
-const pubsub = new PubSub();
+const pubsub = new RedisPubSub({
+  connection: {
+    host: "127.0.0.1",
+    port: 6379,
+    retryStrategy: (options) => Math.max(options.attempt * 100, 3000),
+  },
+});
 
 export default pubsub;
