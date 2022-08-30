8d705e81be8fff73c9f9f98e3aea80382b5e45de
GurshawnL <58784041+GurshawnL@users.noreply.github.com>
2022-08-30 13:07:25 -0400

    modified:   .gitignore 	new file:   secrets/manor-millionaire-admin.js

---
+++ secrets/manor-millionaire-admin.js
@@ -0,0 +1,17 @@
+import { config } from "dotenv";
+const { parsed: envConfig } = config();
+
+const apiKey = {
+  type: envConfig.G_TYPE,
+  project_id: envConfig.G_PROJECT_ID,
+  private_key_id: envConfig.G_PRIVATE_KEY_ID,
+  private_key: envConfig.G_PRIVATE_KEY,
+  client_email: envConfig.G_CLIENT_EMAIL,
+  client_id: envConfig.G_CLIENT_ID,
+  auth_uri: envConfig.G_AUTH_URI,
+  token_uri: envConfig.G_TOKEN_URI,
+  auth_provider_x509_cert_url: envConfig.G_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: envConfig.G_CLIENT_X509_CERT_URL,
+};
+
+export default apiKey;
