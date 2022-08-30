e684048003b5436dfe755de594ab22b597d129bd
GurshawnL <58784041+GurshawnL@users.noreply.github.com>
2022-08-30 14:49:40 -0400

    changed port env

--- index.js
+++ index.js
@@ -87,7 +87,9 @@ async function startApolloServer() {
     app,
   });
 
-  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise((resolve) =>
+    httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
+  );
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
